09769a197c06fa1dfa7c26cd887f8a3c29f8ac92
Miyu Masayama <miyumasayama@Miyus-Laptop.local>
2025-04-15 11:45:07 +0900

    feat/ボタンの追加

---
+++ src/components/commons/primaryButton.tsx
@@ -0,0 +1,42 @@
+import { FC } from "react";
+
+// plain textで処理するため動的クラスを設定することはできない(https://tailwindcss.com/docs/detecting-classes-in-source-files#dynamic-class-names)
+const colors = {
+  red: {
+    default: "bg-red-600",
+    hover: "hover:bg-red-700",
+  },
+  blue: {
+    default: "bg-blue-600",
+    hover: "hover:bg-blue-700",
+  },
+};
+
+type Color = keyof typeof colors;
+
+type Props = {
+  handleClick?: () => void;
+  color?: Color;
+  children: React.ReactNode;
+  type?: "button" | "submit" | "reset";
+  disabled?: boolean;
+};
+
+export const PrimaryButton: FC<Props> = ({
+  handleClick,
+  color = "blue",
+  children,
+  type,
+  disabled = false,
+}) => {
+  return (
+    <button
+      type={type}
+      onClick={handleClick && handleClick}
+      disabled={disabled}
+      className={`w-full ${colors[color].default} ${colors[color].hover} px-4 py-2 text-white font-bold rounded-md transition-colors duration-300 hover:cursor-pointer`}
+    >
+      {children}
+    </button>
+  );
+};
