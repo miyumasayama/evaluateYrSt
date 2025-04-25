8a1955cf0286c0f580cdc59e8a9dbb1fe47ce2bc
Miyu Masayama <miyumasayama@Miyus-Laptop.local>
2025-04-25 16:15:55 +0900

    add/注意書きを追加

--- src/components/stories/create/page_head.tsx
+++ src/components/stories/create/page_head.tsx
@@ -9,9 +9,14 @@ type Props = {
 export const PageHead: FC<Props> = ({ words, reset }) => {
   return (
     <>
-      <h1 className="text-2xl font-bold text-slate-50">
-        ３つの単語を使用してオリジナルの物語を書いてみよう
-      </h1>
+      <div>
+        <h1 className="text-2xl font-bold text-slate-50">
+          ３つの単語を使用してオリジナルの物語を書いてみよう
+        </h1>
+        <p className="text-red-600 text-xs">
+          ※半角文字を使用してください。。文字を正しく検出できない可能性があります。
+        </p>
+      </div>
       <div>
         <ArrowPathIcon
           onClick={() => {
